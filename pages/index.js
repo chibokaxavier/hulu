b1023489fdf76b79f3df51378b1ca15b4759a081
manlikexavy <chibokaxavier@gmail.com>
2023-05-25 16:20:14 -0700

    FETCHING OF DATA IS DONE , UP NEXT IS DISPLAYING THE CONTENTS

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,36 @@ import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import Head from "next/head";
 import Nav from "@/components/Nav";
+import Results from "@/components/Results";
+import requests from "@/utils/requests";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props);
   return (
     <>
       <Head>
         <title>Hulu</title>
       </Head>
-      <Header/>
-    <Nav/>
-      {/* Results */}
+      <Header />
+      <Nav />
+      <Results />
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+  const request = await fetch(
+    `https://api.tmdb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
