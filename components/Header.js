d4151bf826593e2efad1dbfc731fd1a8906cad75
manlikexavy <chibokaxavier@gmail.com>
2023-05-25 10:31:39 -0700

    HEADER COMPLETED UP NEXT IS THE NAV COMPONENT

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import Headeritem from "./Headeritem";
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <Headeritem title="HOME" Icon={HomeIcon} />
+        <Headeritem title="TRENDING" Icon={LightningBoltIcon} />
+        <Headeritem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <Headeritem title="COLLECTIONS" Icon={CollectionIcon} />
+        <Headeritem title="SEARCH" Icon={SearchIcon} />
+        <Headeritem title="ACCOUNT" Icon={UserIcon} />
+      </div>
+      <Image
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}
+        alt=""
+      />
+      
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
